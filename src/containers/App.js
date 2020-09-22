df681d44bed24b2ed8e076057de578063126a57c
Dorlevi <dorlevi93@gmail.com>
2020-09-22 13:12:54 +0300

    dasdad

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,50 @@
-import React, {Component} from 'react';
+import React,{ Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import Scroll from '../components/Scroll';
+
+
 
 class App extends Component {
-	constructor() {
+	constructor(){
 		super();
-		this.state = {
-		robots: [],
+	this.state={
+		robots:[],
 		searchfield: '',
-		}
+
 	}
+}
 
-	componentDidMount(){
-		fetch('http://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({robots: users})
-		);
-	}
+componentDidMount() {
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response=> response.json())
+	.then(users=>this.setState({robots:users})
+	);
+}
 
-
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value})
-	}
-
-	render() {
-		const filterRobots =  this.state.robots.filter(robots => {
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
+onSearchChange=(event)=>{
+	this.setState({ searchfield:event.target.value})
+	
+}
+	render(){
+		const filteredRobots =this.state.robots.filter(robots=>{
+		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+	})
 		return(
 		<div className='tc'>
-			<h1 className='f1'>RoboFriends</h1>
+			<h1 className='f1'>Robotfriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
 			<Scroll>
 				<ErrorBoundry>
-					<CardList robots={filterRobots}/>
+				<CardList robots={filteredRobots}/>
 				</ErrorBoundry>
 			</Scroll>
 		</div>
 		);
 	}
 }
+		
 
 export default App;
